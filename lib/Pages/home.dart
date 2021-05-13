56df2f613b360cc1bd5aceedeb1c077088b0c46c
Michal Barna <misko.barna@gmail.com>
2021-05-13 15:04:56 +0200

    Safearea for header in LV Liste

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -20,11 +20,12 @@ class _HomeState extends State<Home> {
             backgroundColor: Colors.white,
             onPressed: () {}),
         body: ListView(
-            padding: const EdgeInsets.all(8),
-            scrollDirection: Axis.vertical,
-            shrinkWrap: true,
-            children: <Widget>[
-              Padding(
+          padding: const EdgeInsets.all(8),
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          children: <Widget>[
+            SafeArea(
+              child: Padding(
                 padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -39,208 +40,209 @@ class _HomeState extends State<Home> {
                   ],
                 ),
               ),
-              SizedBox(
-                height: 10,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: Container(
+                color: Colors.grey[400],
+                height: 1,
               ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: Container(
-                  color: Colors.grey[400],
-                  height: 1,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 60, 0, 20),
-                child: Text('Your Subjects', style: TextStyle(fontSize: 20, fontFamily: 'Montserrat')),
-              ),
-
-              RaisedButton(
-                onPressed: (){
-                  Navigator.push(
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 60, 0, 20),
+              child: Text('Your Subjects',
+                  style: TextStyle(fontSize: 20, fontFamily: 'Montserrat')),
+            ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.push(
                     context, MaterialPageRoute(builder: (_) => Lv_profile()));
-                },
-                color: Theme.of(context).accentColor,
-                child: Padding(
-                  padding: EdgeInsets.all(1),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        'ADS 1',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.white,
-                        ),
+              },
+              color: Theme.of(context).accentColor,
+              child: Padding(
+                padding: EdgeInsets.all(1),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'ADS 1',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
                       ),
-                      Text(
-                        'SS2021',
-                        style: TextStyle(
-                          fontSize: 17,
-                          fontWeight: FontWeight.w300,
-                          color: Colors.white,
-                        ),
+                    ),
+                    Text(
+                      'SS2021',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
                       ),
-                      Icon(
-                        Icons.favorite,
-                        color: Colors.yellow,
-                      )
-                    ],
-                  ),
+                    ),
+                    Icon(
+                      Icons.favorite,
+                      color: Colors.yellow,
+                    )
+                  ],
                 ),
               ),
-              RaisedButton(
-                onPressed: (){
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (_) => Lv_profile()));
-                },
-                color: Theme.of(context).accentColor,
-                child: Padding(
-                  padding: EdgeInsets.all(1),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        'Datenbanksysteme',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.white,
-                        ),
+            ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (_) => Lv_profile()));
+              },
+              color: Theme.of(context).accentColor,
+              child: Padding(
+                padding: EdgeInsets.all(1),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Datenbanksysteme',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
                       ),
-                      Text(
-                        'SS2021',
-                        style: TextStyle(
-                          fontSize: 17,
-                          fontWeight: FontWeight.w300,
-                          color: Colors.white,
-                        ),
+                    ),
+                    Text(
+                      'SS2021',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
                       ),
-                      Icon(
-                        Icons.favorite,
-                        color: Colors.yellow,
-                      )
-                    ],
-                  ),
+                    ),
+                    Icon(
+                      Icons.favorite,
+                      color: Colors.yellow,
+                    )
+                  ],
                 ),
               ),
-              RaisedButton(
-                onPressed: (){
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (_) => Lv_profile()));
-                },
-                color: Theme.of(context).accentColor,
-                child: Padding(
-                  padding: EdgeInsets.all(1),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        'Programmierung 1 (PR1)',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.white,
-                        ),
+            ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (_) => Lv_profile()));
+              },
+              color: Theme.of(context).accentColor,
+              child: Padding(
+                padding: EdgeInsets.all(1),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Programmierung 1 (PR1)',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
                       ),
-                      Text(
-                        'SS2021',
-                        style: TextStyle(
-                          fontSize: 17,
-                          fontWeight: FontWeight.w300,
-                          color: Colors.white,
-                        ),
+                    ),
+                    Text(
+                      'SS2021',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
                       ),
-                      Icon(
-                        Icons.favorite,
-                        color: Colors.grey,
-                      )
-                    ],
-                  ),
+                    ),
+                    Icon(
+                      Icons.favorite,
+                      color: Colors.grey,
+                    )
+                  ],
                 ),
               ),
-              RaisedButton(
-                onPressed: (){
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (_) => Lv_profile()));
-                },
-                color: Theme.of(context).accentColor,
-                child: Padding(
-                  padding: EdgeInsets.all(1),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        'VU Interoperabilität',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.white,
-                        ),
+            ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (_) => Lv_profile()));
+              },
+              color: Theme.of(context).accentColor,
+              child: Padding(
+                padding: EdgeInsets.all(1),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'VU Interoperabilität',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
                       ),
-                      Text(
-                        'SS2021',
-                        style: TextStyle(
-                          fontSize: 17,
-                          fontWeight: FontWeight.w300,
-                          color: Colors.white,
-                        ),
+                    ),
+                    Text(
+                      'SS2021',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
                       ),
-                      Icon(
-                        Icons.favorite,
-                        color: Colors.grey,
-                      )
-                    ],
-                  ),
+                    ),
+                    Icon(
+                      Icons.favorite,
+                      color: Colors.grey,
+                    )
+                  ],
                 ),
               ),
-              RaisedButton(
-                onPressed: (){
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (_) => Lv_profile()));
-                },
-                color: Theme.of(context).accentColor,
-                child: Padding(
-                  padding: EdgeInsets.all(1),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        'VU Business Intelligence I',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.white,
-                        ),
+            ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (_) => Lv_profile()));
+              },
+              color: Theme.of(context).accentColor,
+              child: Padding(
+                padding: EdgeInsets.all(1),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'VU Business Intelligence I',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
                       ),
-                      Text(
-                        'SS2021',
-                        style: TextStyle(
-                          fontSize: 17,
-                          fontWeight: FontWeight.w300,
-                          color: Colors.white,
-                        ),
+                    ),
+                    Text(
+                      'SS2021',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
                       ),
-                      Icon(
-                        Icons.favorite,
-                        color: Colors.grey,
-                      )
-                    ],
-                  ),
+                    ),
+                    Icon(
+                      Icons.favorite,
+                      color: Colors.grey,
+                    )
+                  ],
                 ),
               ),
-
-              Padding(
-                padding: EdgeInsets.fromLTRB(10, 20, 10, 10),
-                child: Container(
-                  color: Colors.grey[400],
-                  height: 1,
-                ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(10, 20, 10, 10),
+              child: Container(
+                color: Colors.grey[400],
+                height: 1,
               ),
-              Center(child: Text('Total subjects: 5 ', style: TextStyle(fontSize: 13, fontFamily: 'Montserrat')),)
-
-            ],
-        )
-    );
+            ),
+            Center(
+              child: Text('Total subjects: 5 ',
+                  style: TextStyle(fontSize: 13, fontFamily: 'Montserrat')),
+            )
+          ],
+        ));
   }
 }
