73c9cef24deac5bf5e9a00630deb44bff03063f4
Maxim Bogoutdinov <makslp@ymail.com>
2021-05-15 00:18:23 +0200

    feat(review): add all elements

--- lib/Pages/add_review.dart
+++ lib/Pages/add_review.dart
@@ -3,20 +3,89 @@ import 'package:flutter/material.dart';
 import '../Services/user_profile.dart';
 import '../Services/global.dart' as global;
 
-class AddReview extends StatelessWidget {
+class AddReview extends StatefulWidget {
+  @override
+  _AddReviewState createState() => _AddReviewState();
+}
+
+class _AddReviewState extends State<AddReview> {
   User usertmp = User(name: "Peter");
+  TextEditingController editingController = TextEditingController();
+
+  // Style
+  final _listFont = TextStyle(fontSize: 18.0, fontFamily: 'Montserrat');
+  final _starFont = TextStyle(fontSize: 25.0, fontFamily: 'Montserrat');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         floatingActionButton: _getMenuButton(context),
         body: SafeArea(
-            child: ListView(children: <Widget>[
-          _getHeader(),
-          SizedBox(height: 10),
-          _getDivider(),
-          _getLvInfo(),
-          _getLvTeacherInfo()
-        ])));
+            child: ListView(
+                padding: const EdgeInsets.all(8),
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                children: <Widget>[
+              _getHeader(),
+              SizedBox(height: 10),
+              _getDivider(),
+              _getLvInfo(),
+              _getLvTeacherInfo(),
+                  SizedBox(height: 10),
+                  _getDivider(),
+                  _buildRow('Difficulty'),
+                  _buildStars(),
+                  _getDivider(),
+                  _buildRow('Teachers team'),
+                  _buildStars(),
+                  _getDivider(),
+                  _buildRow('Reviews'),
+                  _buildInputBox(),
+                  _saveButton()
+            ])));
+  }
+
+  Widget _buildRow(String title) {
+    return ListTile(
+        title: new Text(title, style: _listFont),
+        );
+  }
+
+  Widget _buildStars() {
+    return ListTile(
+      title: new Text('⭐ ⭐️ ⭐️ ⭐️ ⭐️️', style: _starFont),
+    );
+  }
+
+  Widget _buildInputBox() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        controller: editingController,
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.symmetric(vertical: 60.0),
+          border: OutlineInputBorder(),
+        ),
+      ),
+    );
+  }
+
+  Widget _saveButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+
+            style: ElevatedButton.styleFrom(primary: Colors.white),
+            child: Text("Publish review",
+                style: TextStyle(
+                  color: Colors.black,
+                )))
+      ],
+    );
   }
 
   FloatingActionButton _getMenuButton(context) {
